2a5896fd408111b114ecfd4633ab0e9f2aaa878b
Artemii Kravchuk <artemiykra@gmail.com>
2024-06-18 17:48:30 +0300

    minor refactor

--- main.py
+++ main.py
@@ -8,8 +8,15 @@ import backend.main
 logger = logger.opt(colors=True)
 
 
-if __name__ == "__main__":
-    logger.info("Starting the app...")
-    with open("config.json", "r", encoding="UTF-8") as file:
+def main(config_path: str) -> None:
+    """Run the app (both frontend&backend)"""
+    logger.info("Reading config and running the app..")
+
+    with open(config_path, "r", encoding="UTF-8") as file:
         config = orjson.loads(file.read())
     backend.main.main(config)
+ 
+
+if __name__ == "__main__":
+    logger.info("Starting the app...")
+    main("config.json")
