543a3be84d8d3dd5a640d122b2ff18e5d2943c84
Murad <chelovek477386@gmail.com>
2024-06-21 13:43:18 +0500

    change date format

--- frontend/src/components/Table/Table.jsx
+++ frontend/src/components/Table/Table.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import './Table.css';
 
 const Table = ({ data }) => {
@@ -6,39 +5,52 @@ const Table = ({ data }) => {
         return <p>Loading...</p>;
     }
 
+    const convertTimestampToUserTimezone = (timestamp) => {
+        const date = new Date(timestamp * 1000);
+        return date.toLocaleString('ja-JP', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false
+        }).replace(',', '');
+    };
+
     return (
         <table>
-        <caption>Score table of users</caption>
-        <thead>
-            <tr>
-            <th>id</th>
-            <th>timestamp</th>
-            <th>era</th>
-            <th>username1</th>
-            <th>username2</th>
-            <th>level</th>
-            <th>score</th>
-            <th>country</th>
-            <th>platform</th>
-            <th>mode</th>
-            </tr>
-        </thead>
-        <tbody>
-            {data.map((item) => (
-            <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.timestamp}</td>
-                <td>{item.era}</td>
-                <td>{item.username1}</td>
-                <td>{item.username2}</td>
-                <td>{item.level}</td>
-                <td>{item.score}</td>
-                <td>{item.country}</td>
-                <td>{item.platform}</td>
-                <td>{item.mode}</td>
-            </tr>
-            ))}
-        </tbody>
+            <caption>Score table of users</caption>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>timestamp</th>
+                    <th>era</th>
+                    <th>username1</th>
+                    <th>username2</th>
+                    <th>level</th>
+                    <th>score</th>
+                    <th>country</th>
+                    <th>platform</th>
+                    <th>mode</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((item) => (
+                    <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{convertTimestampToUserTimezone(item.timestamp)}</td>
+                        <td>{item.era}</td>
+                        <td>{item.username1}</td>
+                        <td>{item.username2}</td>
+                        <td>{item.level}</td>
+                        <td>{item.score}</td>
+                        <td>{item.country}</td>
+                        <td>{item.platform}</td>
+                        <td>{item.mode}</td>
+                    </tr>
+                ))}
+            </tbody>
         </table>
     );
 };
